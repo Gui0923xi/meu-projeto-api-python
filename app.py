a74422b98e0d51622105a7e70f8cd98e7f732db2
Gui0923xi <guilhermeoliveleal@gmail.com>
2024-11-14 23:09:46 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 import os
 from flask import Flask, request, jsonify
 import pandas as pd
@@ -35,4 +34,4 @@ def process():
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.ru
